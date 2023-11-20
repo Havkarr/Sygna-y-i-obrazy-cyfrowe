2ad21b87079a9679dc80a6044c93e4eab5cae6d1
Jan Galewicz <jan.galewicz@o2.pl>
2023-11-20 20:03:16 +0100

    Add 2d interpolation

--- lab2.py
+++ lab2.py
@@ -8,6 +8,7 @@ from PIL import Image
 OpenCV korzysta z reprezentacji kolorów BGR zamiast RGB. !!!
 
 Plan dalszych działań:
+- porównać zdjęcia za pomocą odejmowania obrazów
 - Interpolacja koloró R, G, B
 - Pobrać obrazy po interpolacji
 - Suma obrazów
@@ -48,6 +49,33 @@ def linear(x1, y1, x2):
 
                 y2.append(float(temp))
                 break
+
+    return y2
+
+def linear_interpolation(x1, y1, x2):
+    y2 = []
+
+    # Indeks dla poprzedniego punktu w x1
+    prev_index = 0
+
+    for x2_val in x2:
+        # Znajdź indeks dla x2_val w x1, zaczynając od poprzedniego indeksu
+        while prev_index < len(x1) - 1 and x1[prev_index + 1] < x2_val:
+            prev_index += 1
+
+        # Sprawdź czy x2_val znajduje się pomiędzy x1[prev_index] i x1[prev_index + 1]
+        if prev_index < len(x1) - 1:
+            # Oblicz wagi na podstawie odległości nowego punktu od sąsiednich punktów
+            w1 = x1[prev_index + 1] - x2_val
+            w2 = x2_val - x1[prev_index]
+
+            # Oblicz wartość dla punktu z x2 i dodaj ją do listy y2
+            interpolated_value = (y1[prev_index] * w1 + y1[prev_index + 1] * w2) / (w1 + w2)
+
+            y2.append(float(interpolated_value))
+        else:
+            # Jeśli x2_val przekracza największą wartość w x1, użyj ostatniej wartości y1
+            y2.append(float(y1[-1]))
 
     return y2
 
@@ -182,25 +210,24 @@ def cubic_func(x1, y1, x2):
 def main():
     # x1 = np.linspace(0, 5, 10)
     # x2 = np.linspace(0, 5, 100)
-
-    # # Funkcja interpolowana
+    # Funkcja interpolowana
     # y1 = np.sin(x1)
     # y100 = np.sin(x2)
-    # # Interpolacja liniowa
-    # y2 = linear(x1, y1, x2)
-    # # Interpolacja najbliższy-sąsiad
-    # y3 = closest_neightbour(x1, y1, x2)
+    # Interpolacja liniowa
+    #y2 = linear(x1, y1, x2)
+    # Interpolacja najbliższy-sąsiad
+    #y3 = closest_neightbour(x1, y1, x2)
 
-    # # Stworzenie płótna i podzielenie go na sekcje
+    # Stworzenie płótna i podzielenie go na sekcje
     # fig = plt.figure(tight_layout=True)
     # gs = gridspec.GridSpec(2, 2)
 
-    # # # Tworzenie oryginalnego sinusa
-    # # ax = fig.add_subplot(gs[0, :])
-    # # ax.plot(x1, y1, '.r')
-    # # plt.title("Oryginalny sinus")
-    # # ax.set_ylabel('Y')
-    # # ax.set_xlabel('X')
+    # Tworzenie oryginalnego sinusa
+    # ax = fig.add_subplot(gs[0, :])
+    # ax.plot(x1, y1, '.r')
+    # plt.title("Oryginalny sinus")
+    # ax.set_ylabel('Y')
+    # ax.set_xlabel('X')
 
     # # # Tworzenie wykresu interpolacji najbliższy-sąsiad
     # # ax = fig.add_subplot(gs[1, 0])
@@ -216,31 +243,31 @@ def main():
     # # ax.set_ylabel('Y')
     # # ax.set_xlabel('X')
 
-    # # Tworzenie oryginalnego sinusa 100 punktów
+    # Tworzenie oryginalnego sinusa 100 punktów
     # ax = fig.add_subplot(gs[0, :])
     # ax.plot(x1, y1, '.r')
     # plt.title("Oryginalny sinus")
     # ax.set_ylabel('Y')
     # ax.set_xlabel('X')
 
-    # # Tworzenie wykresu interpolacji funkcją kwadratową
+    # Tworzenie wykresu interpolacji funkcją kwadratową
     # y4 = square_func(x1, y1, x2)
-    # ax = fig.add_subplot(gs[1, 0])
-    # ax.plot(x2, y4, '-b')
+    # ax = fig.add_subplot(gs[1, :])
+    # ax.plot(x2, y4, '.b')
     # plt.title("Kwadratowa")
     # ax.set_ylabel('Y')
     # ax.set_xlabel('X')
 
     # # Tworzenie wykresu funkcją sześcienną
     # y5 = cubic_func(x1, y1, x2)
-    # ax = fig.add_subplot(gs[1, 1])
-    # ax.plot(x2, y5, '-g')
+    # ax = fig.add_subplot(gs[1, :])
+    # ax.plot(x2, y5, '.g')
     # plt.title("Sześcienna")
     # ax.set_ylabel('Y')
     # ax.set_xlabel('X')
 
-    # # fig.align_labels()
-    # # plt.show()
+    # fig.align_labels()
+    # plt.show()
 
     with Image.open("image.png") as im:
         photo_array = np.array(im)
@@ -275,9 +302,114 @@ def main():
                                 [[0,0,1], [0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0]],
                                 [[0,0,0], [0,0,0], [0,0,0], [0,0,1], [0,0,0], [0,0,0]]])
 
-        # red = filter(photo_array, red_Fuji_filtr)
-        # green = filter(photo_array, green_Fuji_filtr)
-        # blue = filter(photo_array, blue_Fuji_filtr)
+        red = filter(photo_array, red_Bayer_filtr)
+        green = filter(photo_array, green_Bayer_filtr)
+        blue = filter(photo_array, blue_Bayer_filtr)
+
+        
+
+        # # Działanie na wierszach
+        # temp1 = np.linspace(0, photo_array.shape[1], photo_array.shape[1])
+        # temp2 = np.linspace(0, photo_array.shape[1], photo_array.shape[1])
+
+        # for i in range(1, photo_array.shape[0], 2):
+        #     new_row = linear_interpolation(temp2,red[i, :, 0],temp1)
+        #     for j in range(0,photo_array.shape[1]):
+        #         red[i,j,0] = new_row[j]
+
+        # # Działanie na kolumnach
+        # temp1 = np.linspace(0, photo_array.shape[0], photo_array.shape[0])
+        # temp2 = np.linspace(0, photo_array.shape[0], photo_array.shape[0])
+
+        # for i in range(1, photo_array.shape[1]):
+        #     new_column = linear_interpolation(temp2,red[:, i, 0],temp1)
+        #     for j in range(0,photo_array.shape[0]):
+        #         red[j,i,0] = new_column[j]
+        
+        # Działanie na wierszach
+        temp1 = np.linspace(0, photo_array.shape[1], photo_array.shape[1])
+        temp2 = np.linspace(0, photo_array.shape[1], int(photo_array.shape[1]/2))
+
+        for i in range(1, photo_array.shape[0], 2): 
+            row = []
+            for j in range(0,photo_array.shape[1], 2):
+                row.append(red[i, j, 0])
+            new_row = linear(temp2,row,temp1)
+            red[i,:,0] = new_row
+
+        # Działanie na kolumnach
+        temp1 = np.linspace(0, photo_array.shape[0], photo_array.shape[0])
+        temp2 = np.linspace(0, photo_array.shape[0], int(photo_array.shape[0]/2))
+
+        for i in range(0, photo_array.shape[1]): 
+            column = []
+            for j in range(1,photo_array.shape[0], 2):
+                column.append(red[j, i, 0])
+            new_column = linear(temp2,column,temp1)
+            red[:,i,0] = new_column
+
+        result_image = (red).astype(np.uint8)
+        plt.imshow(result_image)
+        plt.show()
+############################################################################################33
+        # Działanie na wierszach
+        temp1 = np.linspace(0, photo_array.shape[1], photo_array.shape[1])
+        temp2 = np.linspace(0, photo_array.shape[1], photo_array.shape[1])
+
+        for i in range(0, photo_array.shape[0], 1): 
+            row = []
+            for j in range(0,photo_array.shape[1], 1):
+                row.append(green[i, j, 1])
+            new_row = linear(temp2,row,temp1)
+            green[i,:,1] = new_row
+
+        # Działanie na kolumnach
+        temp1 = np.linspace(0, photo_array.shape[0], photo_array.shape[0])
+        temp2 = np.linspace(0, photo_array.shape[0], int(photo_array.shape[0]/2))
+
+        for i in range(0, photo_array.shape[1]): 
+            column = []
+            for j in range(0,photo_array.shape[0]):
+                column.append(green[j, i, 1])
+            new_column = linear(temp2,column,temp1)
+            green[:,i,1] = new_column
+
+        result_image = (green).astype(np.uint8)
+        plt.imshow(result_image)
+        plt.show()
+##################################################################################################
+         # Działanie na wierszach
+        temp1 = np.linspace(0, photo_array.shape[1], photo_array.shape[1])
+        temp2 = np.linspace(0, photo_array.shape[1], int(photo_array.shape[1]/2))
+
+        for i in range(0, photo_array.shape[0], 2): 
+            row = []
+            for j in range(1,photo_array.shape[1], 2):
+                row.append(blue[i, j, 2])
+            new_row = linear(temp2,row,temp1)
+            blue[i,:,2] = new_row
+
+        # Działanie na kolumnach
+        temp1 = np.linspace(0, photo_array.shape[0], photo_array.shape[0])
+        temp2 = np.linspace(0, photo_array.shape[0], int(photo_array.shape[0]/2))
+
+        for i in range(0, photo_array.shape[1]): 
+            column = []
+            for j in range(0,photo_array.shape[0], 2):
+                column.append(blue[j, i, 2])
+            new_column = linear(temp2,column,temp1)
+            blue[:,i,2] = new_column
+        
+        result_image = (blue).astype(np.uint8)
+        plt.imshow(result_image)
+        plt.show()
+
+        rm = red + blue + green
+
+        result_image = (rm).astype(np.uint8)
+        plt.imshow(result_image)
+        plt.show()
+
 
 if __name__ == "__main__":
     main()
